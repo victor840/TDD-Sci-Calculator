c95bc2c069b259abaeb5a680c2c21487a5dafca2
victor840 <victor.ugoanusi@gmail.com>
2020-11-18 21:00:09 -0500

    updated test methods

--- src/main/java/com/codedifferently/tdd/calculator/calculator/basic/BasicCalculatorImp.java
+++ src/main/java/com/codedifferently/tdd/calculator/calculator/basic/BasicCalculatorImp.java
@@ -5,43 +5,43 @@ import com.codedifferently.tdd.calculator.calculator.memory.MemoryImplementation
 
 import static com.codedifferently.tdd.calculator.utils.CustomConsole.*;
 
-public class BasicCalculatorImp implements Calculator{
+public class BasicCalculatorImp implements Calculator {
     protected Memory memory;
 
-    public BasicCalculatorImp(){
+    public BasicCalculatorImp() {
         this.memory = new MemoryImplementation();
     }
 
-    public void run(){
+    public void run() {
         String message = "";
         println("Welcome to TDD Calculator");
         message = String.format("The current Value is %d", memory.recallCurrentValue());
         println(message);
     }
 
-    protected Double add(Double input){
+    protected Double add(Double input) {
         Double sum = input + memory.recallCurrentValue();
         memory.setCurrentValue(sum);
         return sum;
     }
 
-    protected Double subtract(Double input){
+    protected Double subtract(Double input) {
         Double minus = memory.recallCurrentValue() - input;
         memory.setCurrentValue(minus);
         return minus;
     }
 
-    protected Double multiply(Double input){
+    protected Double multiply(Double input) {
         Double product = memory.recallCurrentValue() * input;
         memory.setCurrentValue(product);
         return product;
     }
 
-    protected Double divide(Double input){
+    protected Double divide(Double input) {
         return null;
     }
 
-    protected Double sq(Double input){
+    protected Double sq(Double input) {
         return null;
     }
 
@@ -49,3 +49,4 @@ public class BasicCalculatorImp implements Calculator{
         return null;
     }
 }
+//pushing to master branch
