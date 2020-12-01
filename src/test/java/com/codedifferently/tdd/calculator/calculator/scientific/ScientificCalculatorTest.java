931c1f62d1aa961b50504703c4d280f1ad217fad
victor840 <victor.ugoanusi@gmail.com>
2020-12-01 17:21:28 -0500

    Updated log test

--- src/test/java/com/codedifferently/tdd/calculator/calculator/scientific/ScientificCalculatorTest.java
+++ src/test/java/com/codedifferently/tdd/calculator/calculator/scientific/ScientificCalculatorTest.java
@@ -1,12 +1,32 @@
 package com.codedifferently.tdd.calculator.calculator.scientific;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+
 public class ScientificCalculatorTest {
+
+    SciCalculator sciCalculator;
+
+    @Before
+    public void setUp(){
+        sciCalculator = new SciCalculator();
+    }
+
     @Test
     public void logTest(){
         //Given
-        SciCalculator sciCalculator = new SciCalculator();
+        sciCalculator.memory.setCurrentValue(1.0);
+
+        //When
+
+        Double expected = 0.0;
+        Double actual = sciCalculator.log(1.0);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
 
 
 
